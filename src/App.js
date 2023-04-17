c6d92d2bad26cda0c23738cba32f2b91ac49394a
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-04-17 17:05:58 +0700

    create router app.js
    preview: https://imgur.com/QX5kiuT
    
    router to /about : https://imgur.com/8RWIdsq
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+    <BrowserRouter>
+    <Routes>
+     <Route path='/' element={<Layout/>}>
+      <Route index element={<Home></Home>}></Route>
+      <Route path='about' element={<About/>}></Route>
+      <Route path='contact' element={<Contact/>}></Route>
+     </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
