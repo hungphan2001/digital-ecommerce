45f11b77abd29a5367eddac1755de3fa0e82e0f1
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-06-03 02:54:34 +0700

    forgot password confirm via gmail
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/pages/Forgotpassword.js
+++ src/pages/Forgotpassword.js
@@ -1,10 +1,30 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { forgotPasswordToken } from "../features/user/userSlice";
+
+const EmailSchema = yup.object({
+  email: yup.string().email("Invalid email").required("Email Required"),
+});
 const Forgotpassword = () => {
+  const navigate = new useNavigate();
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: EmailSchema,
+    onSubmit: (values) => {
+      dispatch(forgotPasswordToken(values));
+      navigate("/");
+    },
+  });
   return (
     <>
       <Meta title={"Forgot Password"} />
@@ -17,8 +37,21 @@ const Forgotpassword = () => {
               <p className="text-center mt-2 mb-3">
                 We will send you an email to reset your password
               </p>
-              <form action="" className="d-flex flex-column gap-15">
-                <CustomInput type="email" name="email" placeholder="Email" />
+              <form onClick={formik.handleSubmit}
+              action="" className="d-flex flex-column gap-15">
+                <CustomInput
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange("email")}
+                  onBlur={formik.handleBlur("email")}
+                />
+                <div className="error">
+                    {
+                      formik.touched.email && formik.errors.email
+                    }
+                </div>
 
                 <div>
                   <div className="mt-3 d-flex justify-content-center flex-column gap-15 align-items-center">
